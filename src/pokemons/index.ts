9c4aaa4d27cf84f92eb15b96e5f6a649022adba9
Adrian Martin Barcelo <adrian.m.barcelo@gmail.com>
2024-05-14 14:57:58 +0200

    Add PokemonGrid component and lazy loading for images

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,5 +1,7 @@
 
+
 export { PokemonGrid } from './components/PokemonGrid';
 
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SinglePokemon } from './interfaces/single-pokemon';
+export type { Pokemon } from './interfaces/pokemon';
